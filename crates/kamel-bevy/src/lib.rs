87c14737d6c63656ad611b1ae77567b90eebe31c
BeastLe9enD <beastle9end@outlook.de>
2022-06-17 14:55:33 +0200

    ✨ Add first window

---
+++ crates/kamel-bevy/src/lib.rs
@@ -0,0 +1,27 @@
+pub mod app {
+    pub use bevy_app::*;
+}
+
+pub mod core {
+    pub use bevy_core::*;
+}
+
+pub mod ecs {
+    pub use bevy_ecs::*;
+}
+
+pub mod input {
+    pub use bevy_input::*;
+}
+
+pub mod log {
+    pub use bevy_log::*;
+}
+
+pub mod window {
+    pub use bevy_window::*;
+}
+
+pub mod winit {
+    pub use bevy_winit::*;
+}
--- crates/kamel-game/src/main.rs
+++ crates/kamel-game/src/main.rs
@@ -1,3 +1,32 @@
+use kamel_bevy::{
+    app::{App, PluginGroup, PluginGroupBuilder},
+    core::CorePlugin,
+    input::InputPlugin,
+    log::LogPlugin,
+    window::{WindowDescriptor, WindowPlugin},
+    winit::WinitPlugin
+};
+
+struct DefaultPlugins;
+
+impl PluginGroup for DefaultPlugins {
+    fn build(&mut self, group: &mut PluginGroupBuilder) {
+        group.add(LogPlugin::default());
+        group.add(CorePlugin::default());
+        group.add(InputPlugin::default());
+        group.add(WindowPlugin::default());
+        group.add(WinitPlugin::default());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: 1600.0,
+            height: 900.0,
+            title: "Kamel Game".to_string(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
